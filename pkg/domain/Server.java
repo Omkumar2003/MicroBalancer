4c4c9612e8daefd5296b150c65dc0e012ad61a52
OmKumarGithub <omkumar742003@gmail.com>
2024-07-17 12:33:32 +0530

    added sync

--- pkg/domain/Server.java
+++ pkg/domain/Server.java
@@ -6,33 +6,70 @@ import java.net.http.*;
 // import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import helper.ReverseProxy;
+
 public class Server {
     URL url;
-    // Right now I don't know how to set up reverse proxy my guess is I have to use
-    // some third party software
+    static ReverseProxy proxy;
     HashMap<String, String> metaData;
     boolean alive;
     ReentrantReadWriteLock mu;
 
     public static void Forward(HttpResponse res, HttpRequest req) {
-
+        proxy.serveHttp();
     }
 
     public String GetMetaOrDefault(String key, String defVal) {
-        boolean temp = false;
         String val = defVal;
-        metaData.forEach((k, v) -> {
-            if (k == key) {
-                val = v;
-                temp = true;
-                return;
+        for (Map.Entry<String, String> entry : metaData.entrySet()) {
+            if (entry.getKey().equals(key)) {
+                val = entry.getValue();
+                break;
             }
-        });
-        return defVal;
+        }
+        return val;
     }
 
+    public int GetMetaOrDefaultInt(String key, int defaultVal) {
+        String temp = "" + defaultVal + "";
+        String val = GetMetaOrDefault(key, temp);
+        try {
+            int temp1 = Integer.parseInt(val);
+            return temp1;
 
-    
+        } catch (NumberFormatException e) {
+            return defaultVal;
+        }
+
+    }
+
+    public boolean setLiveness(boolean value) {
+        try {
+            mu.writeLock().lock();
+            alive = value;
+            // return alive;
+        } catch (Exception e) {
+        } finally {
+            mu.writeLock().unlock();
+        }
+        return alive;
+
+    }
+
+    public boolean isAlive() {
+
+        try {
+            mu.readLock().lock();
+        } catch (Exception e) {
+        } finally {
+            mu.readLock().unlock();
+        }
+        ;
+
+        return alive;
+
+    }
+
 }
 
 class Replica {
